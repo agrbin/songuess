4e0592bad72b457bbaa8ffc3aa789aa9c0e14b01
Anton Grbin <anton.grbin@fer.hr>
2013-08-19 21:29:01 +0200

    written doc for auth
    can be reused.

--- auth.js
+++ auth.js
@@ -1,12 +1,41 @@
 /*
- * if mustHaveCookie is set to true, class will onError
- * if cookie is not available trough filesystem API
+ * Auth will communicate with google to obtain acces token that can be
+ * verified with server. location.hash will remain unchanged in process.
+ * ClientID and scope can be changed in OAuthParams var at the top of the
+ * class.
+ * This class have myClock for dependency.
+ *
+ * Constructor:
+ * @param storage is object with interface:
+ *  writeFile (file, data done)
+ *  isFile (file, yes, no)
+ *  readFile (file, done)
+ *  killFile (file, done)
+ *
+ * @param onReady callback to invoke when access token is obtained.
+ *  onReady (token); token is object with fields
+ *    - access_token
+ *    - expires_in
+ *
+ * @param isOAuthReturn boolean, if set to false Auth will fail if access token
+ *  can't be loaded from storage. This is used when instantiating class from a
+ *  location that is not registered in google apis.
+ *
+ * @onError
+ *  will be invoked with error text message is something goes south.
+ *
+ *
+ * Methods:
+ * @method kill(done)
+ *  will kill the token from storage and invoke done afterwards.
+ *
+ * @method verifyToken(ws, done)
+ *  will send token over WebSocket and receive user data extracted from that
+ *  token. Standard done convention is used: done(result, err);
  */
 function Auth(storage, onReady, isOAuthReturn, onError) {
 
   var that = this,
-    COOKIE_FILE_NAME = "cookie",
-    token,
     oAuthParams = {
       response_type : 'token',
       client_id     : '156401229517.apps.googleusercontent.com',
@@ -15,49 +44,41 @@ function Auth(storage, onReady, isOAuthReturn, onError) {
         'https://www.googleapis.com/auth/userinfo.email' +
         ' https://www.googleapis.com/auth/userinfo.profile',
       state : location.hash || ""
-    };
+    },
+    COOKIE_FILE_NAME = "cookie",
+    token;
 
-  this.kill = function(done) {
-    storage.killFile(COOKIE_FILE_NAME, done);
-  };
+  function initialize() {
+    storage.isFile(
+      COOKIE_FILE_NAME,
+      cookieFileExists,
+      initiateHandshake
+    );
+  }
 
-  this.verifyToken = function(ws, onVerified) {
-    ws.onmessage = function(msg) {
-      ws.onclose = null;
-      onVerified(JSON.parse(msg.data));
-    };
-    ws.onclose = function(e) {
-      that.kill();
-      onError("server couldn't verify token: " + e.reason);
-    };
-    ws.send(JSON.stringify(token));
-  };
+  // if cookie file exists, check whether it's valid.
+  function cookieFileExists() {
+    debug("cookie file exists.");
+    storage.readFile(COOKIE_FILE_NAME, function(filedata) {
+      var data;
+      function bailOut() {
+        debug("cookie file is not valid.");
+        storage.killFile(COOKIE_FILE_NAME, initiateHandshake);
+      }
+      try {data = JSON.parse(filedata);}
+      catch (e) {return bailOut();}
+      if (!data) return bailOut();
+      if (!('expires' in data)) return bailOut();
+      if (data.expires < myClock.originalClock()) return bailOut();
+      token = data;
+      haveToken();
+    });
+  }
 
   function haveToken() {
     debug("token obtained.");
     onReady(token);
   };
-
-  storage.isFile(COOKIE_FILE_NAME,
-    function() {
-      debug("cookie file exists.");
-      storage.readFile(COOKIE_FILE_NAME, function(filedata) {
-        var data;
-        function bailOut() {
-          debug("cookie file is not valid.");
-          storage.killFile(COOKIE_FILE_NAME, initiateHandshake);
-        }
-        try {data = JSON.parse(filedata);}
-        catch (e) {return bailOut();}
-        if (!data) return bailOut();
-        if (!('expires' in data)) return bailOut();
-        if (data.expires < myClock.originalClock()) return bailOut();
-        token = data;
-        haveToken();
-      });
-    },
-    initiateHandshake
-  );
 
   // checks if this is redirected page already
   function checkFragment() {
@@ -77,6 +98,7 @@ function Auth(storage, onReady, isOAuthReturn, onError) {
     }
   }
 
+  // redirects to oauth endpoint.
   function initiateHandshake() {
     if (!isOAuthReturn) {
       onError("initiating handshake from isOAuthReturn =="
@@ -85,15 +107,32 @@ function Auth(storage, onReady, isOAuthReturn, onError) {
       return;
     }
     if (!checkFragment()) {
-      debug("redirectam se na oauth");
-      // redirects to google oauth endpoint
-      var url = "https://accounts.google.com/o/oauth2/auth?";
-      var fragments = [];
-      for (var field in oAuthParams)
+      debug("redirecting to oauth endpoint");
+      var url = "https://accounts.google.com/o/oauth2/auth?",
+        fragments = [], field;
+      for (field in oAuthParams)
         fragments.push(field + '=' + escape(oAuthParams[field]));
       document.location = url + fragments.join("&");
     }
   }
+
+  this.kill = function(done) {
+    storage.killFile(COOKIE_FILE_NAME, done);
+  };
+
+  this.verifyToken = function(ws, onVerified) {
+    ws.onmessage = function(msg) {
+      ws.onclose = null;
+      onVerified(JSON.parse(msg.data));
+    };
+    ws.onclose = function(e) {
+      that.kill();
+      onError("server couldn't verify token: " + e.reason);
+    };
+    ws.send(JSON.stringify(token));
+  };
+
+  initialize();
 
 }
 
