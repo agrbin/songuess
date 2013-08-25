0a59b0a554413ed3ea74159e7bbd6d0eaefac2f5
Anton Grbin <anton.grbin@fer.hr>
2013-08-25 12:43:23 +0200

    bugfix in deploy
    config.js.override didn't restore properly

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -8,7 +8,9 @@ echo "pulling changes.. watch out for /server/config.js changes!!";
 # git pull
 
 echo "minifying client..";
+
 ( cd $root/client && node ../deploy/minify.js )
+
 mv $root/client/index.min.html $root/server/
 
 # jitsu.config.js -> ../server/config.override.js
@@ -16,11 +18,14 @@ mv $root/client/index.min.html $root/server/
 # restore ../server/config.override.js
 echo "deploying to nodejitsu..";
 if [ -f $root/server/config.override.js ]; then
+  echo "postoji.";
   mv $root/server/config.override.js $root/server/config.override.js.backup
 fi
 cp $root/deploy/jitsu.config.js $root/server/config.override.js
 ( cd $root/server && jitsu deploy --confirm )
+rm $root/server/config.override.js
 if [ -f $root/server/config.override.js.backup ]; then
+  echo "postoji.";
   mv $root/server/config.override.js.backup $root/server/config.override.js
 fi
 rm -f $root/server/index.min.html
