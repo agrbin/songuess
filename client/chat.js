7dc63464ad2de18c1b04545ea712d0d9b2813b39
Anton Grbin <anton.grbin@carnet.hr>
2015-11-01 20:18:31 +0000

    updatean help

--- client/chat.js
+++ client/chat.js
@@ -132,7 +132,8 @@ function Chat(wsock, user, media, player, onFatal) {
     ui.addNotice("Available commands are ");
     ui.addNotice("- /clear, /join #room, /mute, /vol [0-10]");
     ui.addNotice("- /sync, /reset, /who [#room], /group [0,1,2,...]");
-    ui.addNotice("- /playlist, /stream");
+    ui.addNotice("- /playlist, /stream, /info, /next");
+    ui.addNotice("- /reset (will reset your score), /honor");
   });
 
   onCommand("hello", function () {
